f0b673824579c66e6bd16d248f222c35e8298b80
戸軽健太 <n99.kenta.togaru@gmail.com>
2024-06-10 13:37:27 +0900

    push時lint testが走るかの確認

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,6 @@ function App() {
   const [count, setCount] = useState(0);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1 className="text-3xl text-blue-500">WoodSticks</h1>
       <div className="card">
         <Button onClick={() => setCount((count) => count + 1)}>

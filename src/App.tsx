d2c3d0bf82a721a6a4aa338d6ef96ed3198b0aa8
戸軽健太 <n99.kenta.togaru@gmail.com>
2024-06-10 15:29:34 +0900

    Add pre-push hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,6 @@ import  { useState } from "react";
 import "./App.css";
 import { Button } from "./components/ui/button";
 
-
-
-
 function App() {
   const [count, setCount] = useState(0);
   return (
